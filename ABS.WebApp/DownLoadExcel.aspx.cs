4645c6a201494491a6a1e1a7c1695cc6e30597a4
shourya reddy kommula <shouryak12@shouryas-MBP.lan>
2024-04-16 13:57:43 -0400

    Initialized Git Repo

---
+++ ABS.WebApp/DownLoadExcel.aspx.cs
@@ -0,0 +1,35 @@
+﻿using ABS.FileGeneration;
+using System;
+using System.IO;
+
+namespace ABS.WebApp
+{
+    public partial class DownLoadExcel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileGenerationService obj = new FileGenerationService();
+                MemoryStream mstream = obj.GenerateFileAsync().Result;
+                byte[] byteArray = mstream.ToArray();
+                mstream.Flush();
+                mstream.Close();
+                Response.Clear();
+                Response.AddHeader("Content-Disposition", "attachment; filename=Test.xlsx");
+                Response.AddHeader("Content-Length", byteArray.Length.ToString());
+                Response.ContentType = "application/octet-stream";
+                Response.BinaryWrite(byteArray);
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Something went wrong :" + ex.Message + "Trace :" + ex.StackTrace);
+            }
+        }
+    }
+}
